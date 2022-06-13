a19ca36bd2ffd48120fed4436fb579cf27702be9
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-06-13 23:56:01 +0530

    Add files via upload

--- Pygame Flappy run/Flappy game.py
+++ Pygame Flappy run/Flappy game.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 pygame.init()#initializing pygame
-screen=pygame.display.set_mode((800,480),pygame.RESIZABLE)#creating a display surface
+screen=pygame.display.set_mode((800,480))#pygame.RESIZABLE)#creating a display surface
 screen.fill("Green")
 pygame.display.set_caption("Flappy run")#name to be displayed on top of window
 
@@ -21,15 +21,24 @@ rosemary_surface=pygame.image.load("graphics/rosemary.png")
 
 test_font=pygame.font.Font(None,50)
 text_surface=test_font.render("Flappy Run",True,"Yellow")
+text_rect=text_surface.get_rect(center=(395,65))
+
+pygame.draw.rect(bg_surface,'Green',text_rect,10)
+pygame.draw.rect(bg_surface,'Green',text_rect)
 #if the attribute is true we get normal text if false we get text in pixels
 
-snail_surface=pygame.Surface((72,36))
+#snail_surface=pygame.Surface((72,36))
 snail_surface=pygame.image.load("graphics/snail1.png")
-snail_xcoordinate=800
+snail_rect=snail_surface.get_rect(midbottom=(800,300))
+#snail_xcoordinate=800
 
-fox_surface=pygame.Surface((72,36))
+#fox_surface=pygame.Surface((72,36))
 fox_surface=pygame.image.load("graphics/fox.png")
+fox_rect=fox_surface.get_rect(midbottom=(0,300))
 fox_xcoordinate=0
+
+player_surface=pygame.image.load("graphics/player_stand.png")
+player_rect=player_surface.get_rect(midbottom =(10,300))
 
 while True:
     for event in pygame.event.get():#to all the possible events
@@ -41,17 +50,33 @@ while True:
     screen.blit(text_surface,(300,50))
     screen.blit(rosemary_surface,(0,280))
     screen.blit(rosemary_surface,(750,280))
+    player_rect.left+=1
+    screen.blit(player_surface,player_rect)
 
-    snail_xcoordinate-=1
-    if snail_xcoordinate==0:
-        snail_xcoordinate=800
-    screen.blit(snail_surface,(snail_xcoordinate,270))
+    snail_rect.left-=1
+    if snail_rect.left==0:
+        snail_rect.right=800
+        #snail_rect.left-=1
+    screen.blit(snail_surface,snail_rect)
 
-    fox_xcoordinate+=1
-    if fox_xcoordinate==800:
-        fox_xcoordinate=0
-    screen.blit(fox_surface,(fox_xcoordinate,270))
+    fox_rect.left+=1
+    if fox_rect.right==800:
+        fox_rect.left=0
+        #fox_rect.left+=1
+    screen.blit(fox_surface,fox_rect)
 
+    """if fox_rect.right==snail_xcoordinate:
+        text_surface=test_font.render("The end",True,"Yellow")
+        screen.blit(text_surface,(300,50))"""
+    mouse_posi=pygame.mouse.get_pos()
+    if player_rect.collidepoint(mouse_posi):
+        print(pygame.mouse.get_pressed())
+        
+    '''if player_rect.colliderect(snail_rect)==1:
+        print("Collision")'''
+    
+
+        
     #BLIT=block image tansfer puting regular on display
                #name of surface,position  200px from left,100px from top      
     pygame.display.update()#for updating the initial display surface
@@ -62,4 +87,18 @@ while True:
 """There are 2 types of sufaces in pygame
 DISPLAY SURFACE =Actual window that is displayed,the game window(unique)
 REGULAR SURFACE=A single img or plain color
-It needs to be put on the display surface to be visible(flexible ammount)"""
+It needs to be put on the display surface to be visible(flexible ammount)
+
+rectanges are used in pygame for precise positioning in screen,basic collisions
+
+TO DISPLAY THE MOUSE POSI:
+In the game's main loop create an if condition for this event:
+if event.type=pygame.MOUSEMOTION:
+    print(event.pos)
+
+if the user cliks up button:
+    if event.type=pygame.MOUSEUP:
+        print("*")
+
+To draw a variable line
+pygame.draw.line(screen,'Gold',(0,0),pygame.mouse.get_pos(),10"""
